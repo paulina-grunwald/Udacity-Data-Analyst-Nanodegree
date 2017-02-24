61d4f51129b8a1e057706ed6f9c8aee9a3d13ba0
paulina-grunwald <paulina.grunwald@gmail.com>
2017-02-24 13:18:41 +0100

    update

--- Project 6 - Make Effective Data Visualization/Final project/js/main.js
+++ Project 6 - Make Effective Data Visualization/Final project/js/main.js
@@ -1,3 +1,4 @@
+
 // header
     var Title = d3.select('#title')
                                 .append("h2")
@@ -9,60 +10,31 @@
                                 .append("h3")
                                 .text("CRMS titanic was a British passenger liner that sank in the North Atlantic Ocean in the early morning of 15 April 1912, after colliding with an iceberg during her maiden voyage from Southampton to New York City. Of the 2,224 passengers and crew aboard, more than 1,500 died in the sinking.")
                                 .attr("align","center");
-                                
-// 1 - Titanic survivors based on class
-    var width = 590,
-        height = 400;
-    var svg1 = dimple.newSvg("#chart1", width, height);
-    d3.csv("titanic.csv", function(data){
-        var chart = new dimple.chart(svg1, data);
-        chart.addCategoryAxis("x", ["Class","Sex"]);
-        chart.addMeasureAxis("y", "Survived");
-        chart.addSeries("Sex", dimple.plot.bar);
-        svg1.append("text")
-         .attr("x", chart._xPixels() + chart._widthPixels() / 2)
-         .attr("y", chart._yPixels() - 20)
-         .style("text-anchor", "middle")
-         .style("font-weight", "bold")
-         .text("Survival Rate per class ");
-    chart.addLegend(65, 10, 510, 20, "right");
+       
+
+
+
+//Chart 1
+    var svg = dimple.newSvg("#chart1", 590, 400);
+    d3.tsv("data/titanic.csv", function (data) {
+              var chart = new dimple.chart(svg, data);
+              chart.addCategoryAxis("x", ["Class","Survival"]);
+              chart.addMeasureAxis("y", "Count");
+              chart.addSeries("Survival", dimple.plot.bar);
+              chart.addLegend(65, 10, 510, 20, "right");
+              svg.append("text")
+              // Position in the center of the shape (vertical position is
+              // manually set due to cross-browser problems with baseline)
+                .attr("x", chart._xPixels() + chart._widthPixels() / 2)
+                .attr("y", chart._yPixels() - 20)
+                 // Align to center
+                .style("text-anchor", "middle")
+                .style("font-weight", "bold")
+                .style("font-family", "sans-serif")
+                .text("Survival Rate per class ")
     chart.draw();
+    
     });
+ 
 
-    // 2 - Titanic survivors based on age group
-    var width = 590,
-        height = 400;
-    var svg2 = dimple.newSvg("#chart2", width, height);
-    d3.csv("data/survival_age_group.csv", function(data){
-        var chart = new dimple.chart(svg2, data);
-        x = chart.addCategoryAxis("x", "Age Group");
-        x.addOrderRule(["N/A", "0-15", "15-30", "30-45", "45-60", "60+"]);
-        chart.addMeasureAxis("y", "Survival Rate");
-        chart.addSeries(null,dimple.plot.bar);
-        svg2.append("text")
-         .attr("x", chart._xPixels() + chart._widthPixels() / 2)
-         .attr("y", chart._yPixels() - 20)
-         .style("text-anchor", "middle")
-         .style("font-weight", "bold")
-         .text("Survival Rate per age group");
-    chart.draw();
-    }); 
 
-    // 3 - Titanic survivors based on parents/children 
-    var width = 590,
-        height = 400;
-    var svg3 = dimple.newSvg("#chart3", width, height);
-    d3.csv("data/survival_parc.csv", function(data){
-        var chart = new dimple.chart(svg3, data);
-        x = chart.addCategoryAxis("x", "Parch");
-        chart.addMeasureAxis("y", "Survival Rate");
-        chart.addSeries(null, dimple.plot.bar);
-        svg3.append("text")
-         .attr("x", chart._xPixels() + chart._widthPixels() / 2)
-         .attr("y", chart._yPixels() - 20)
-         .style("text-anchor", "middle")
-         .style("font-weight", "bold")
-         .text("Survival Rate split by parents with children");
-      chart.draw();
-      x.titleShape.text("Parents with children");
-    });    
